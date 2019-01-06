5f360a8d3f240e0362205f6c327a46b90919cbf1
clive <francoclivemaleke23@gmail.com>
2019-01-06 14:51:39 +0700

    final

--- app.py
+++ app.py
@@ -1,43 +1,78 @@
 #---------- Module ----------#
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
+import pymysql
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from os import path, getcwd
 from PIL import Image
 import pytesseract
+import db_config
+import time
+
+
 
 app = Flask(__name__)
+app.config['file_allowed'] = ['image/png', 'image/jpeg']
 app.config['image_storage'] = path.join(getcwd(), 'image_storage')
-# db = SQLAlchemy()
-
 
 @app.route('/', methods=['GET'])
 def index():
-    print (pytesseract.image_to_string(Image.open('test.png')))
-    return('index')
+    try:
+        conn = db_config.mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM file')
+        rows = cursor.fetchall()
+        return jsonify( id = 1,
+                        status = 'OK',
+                        msg = 'Saved',
+                        results = rows,
+                        error = None
+                        ), 200
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
 
 @app.route('/v1/upload', methods=['POST'])
 def upload():
     try:
         img_form = request.files['filename']
-        filename = secure_filename(img_form.filename)
+        filename = str(time.time()) + "_" + secure_filename(img_form.filename)
         local_storage = path.join(app.config['image_storage'])
         img_form.save(path.join(local_storage, filename))
-        convert = pytesseract.image_to_string(Image.open(filename))
-        print(convert)
-        return jsonify( id = 1,
-                        status = 'OK',
+        convert = pytesseract.image_to_string(Image.open(path.join(local_storage, filename)))
+
+        # sql command
+        conn = db_config.mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "INSERT INTO file (path, description) VALUES (%s, %s)"
+        value = (filename, convert)
+        cursor.execute(sql, value)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("ok")
+
+        return jsonify( status = 'OK',
                         msg = 'Saved',
-                        results = filename,
+                        data = filename,
                         error = None
                         ), 201
-    except:
+    except Exception as e:
+        print(e)
         return jsonify( id = 1,
                         status = 'FAILED',
                         msg = 'failed',
-                        results = 'no data saved',
+                        data = 'no data saved',
                         error = True
                         ), 422
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['image_storage'],
+                               filename)
 
 
 #----------- Run the APP ----------#
